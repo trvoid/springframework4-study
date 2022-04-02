519a9a81baffbc4a6805285b7b21fefd979be956
trvoid <wayne.jeong.wj@gmail.com>
2022-04-02 20:15:30 +0900

    Initial commit.

---
+++ src/main/java/trvoid/executor/ConsoleExecutor.java
@@ -0,0 +1,12 @@
+package trvoid.executor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConsoleExecutor {
+    private static Logger LOGGER = LoggerFactory.getLogger(ConsoleExecutor.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello World!!!");
+    }
+}
